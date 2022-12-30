fbe52cbaf6cdf62386aca5d2523dafd21d5dd0ee
landeroov <meljohn7.mm@gmail.com>
2022-12-30 09:47:35 +0800

    React JS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,37 @@
-import { useState } from 'react'
-import './indexs.css'
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Public from './components/Public'
+import Login from './features/auth/Login'
+import Welcome from './features/auth/Welcome'
+import NotesList from './features/notes/NotesList'
+import UsersList from './features/users/UsersList'
+import DashLayout from './components/DashLayout'
 
 function App() {
-
   return (
-    <div className="app">
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Public />}/>
+        <Route path="login" element={<Login />} />
+
+        {/*START DASH*/}
+        <Route path="dash" element={<DashLayout />}>
+
+          <Route index element={<Welcome />} />
+
+          <Route path="notes">
+            <Route index element={<NotesList />} />
+          </Route>
+
+          <Route path="users">
+            <Route index element={<UsersList />} />
+          </Route>
+
+        </Route>
+        {/*END OF DASH*/}
+        
+      </Route>
+    </Routes>
   )
 }
 
---
+++ client/src/components/DashFooter.jsx
@@ -0,0 +1,34 @@
+import { AiFillHome } from 'react-icons/ai'
+import { useNavigate, useLocation } from 'react-router-dom'
+
+const DashFooter = () => {
+
+	const navigate = useNavigate()
+	const { pathname } = useLocation()
+
+	const onGoHomeClicked = () => navigate('/dash')
+
+	let goHomeButton = null
+	if(pathname !== '/dash') {
+		goHomeButton = (
+			<button
+				className="dash-footer__button icon-button"
+				title="Home"
+				onClick={onGoHomeClicked}
+			
+			><AiFillHome /></button>
+		)
+	}
+
+	const content = (
+		<footer className="dash-footer">
+			{goHomeButton}
+			<p>Current User:</p>
+			<p>Status:</p>
+		</footer>
+	)
+
+	return content
+}
+
+export default DashFooter
