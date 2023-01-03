7e6078c4c2c1e66bd131bcaa241b81f51f98e7a0
landeroov <meljohn7.mm@gmail.com>
2023-01-03 19:15:52 +0800

    Login Auth

--- client/src/app/store.jsx
+++ client/src/app/store.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit"
 import { apiSlice } from './api/apiSlice'
 import { setupListeners } from '@reduxjs/toolkit/query' //prevents stale data if there is more than one instance of user visiting the page
+import authReducer from '../features/auth/authSlice'
 
 export const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
+        auth: authReducer,
     },
     middleware: getDefaultMiddleware =>
         getDefaultMiddleware().concat(apiSlice.middleware),
