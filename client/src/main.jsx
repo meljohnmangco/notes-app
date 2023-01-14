1c4ed5743eebfdefd5af72f0706e0a3d52b5ef4e
landeroov <meljohn7.mm@gmail.com>
2023-01-14 12:29:48 +0800

    ready to deploy

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
 import { store } from './app/store'
 import { Provider } from 'react-redux'
- 
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+
+if(process.env.NODE_ENV === 'production') disableReactDevTools()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
