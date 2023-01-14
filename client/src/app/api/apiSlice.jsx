30d2bea0c84f12eaf7ccedff46dd88728ea4361b
landeroov <meljohn7.mm@gmail.com>
2023-01-14 13:05:05 +0800

    fectchbasequery

--- client/src/app/api/apiSlice.jsx
+++ client/src/app/api/apiSlice.jsx
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://notes-app-api.onrender.com',
+    baseUrl: 'https://notes-app-api-g5ge.onrender.com/',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
